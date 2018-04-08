e43815b00b7ca0b28d84f19648b5d8b11930447d
sinde <sinde12@163.com>
2018-04-08 16:37:43 +0800

    sinde

--- baselibrary/src/main/java/hohistar/sinde/baselibrary/base/AbstractBaseActivity.kt
+++ baselibrary/src/main/java/hohistar/sinde/baselibrary/base/AbstractBaseActivity.kt
@@ -1,9 +1,12 @@
 package hohistar.sinde.baselibrary.base
 
 import android.app.Activity
+import android.os.Bundle
 
 class AbstractBaseActivity : Activity(){
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
 }
